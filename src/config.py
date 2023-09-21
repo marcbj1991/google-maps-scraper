c543ca8613be3b5dd928a793d930be3f132eb6e6
marcbj1991 <61287042+marcbj1991@users.noreply.github.com>
2023-09-21 09:44:45 +0200

    up

--- src/config.py
+++ src/config.py
@@ -1,11 +1,228 @@
-'''
-Love It? Star It! ⭐ https://github.com/omkarcloud/google-maps-scraper/
-'''
 queries = [
     {
-        "keyword": "restaurants in delhi",
-        "max_results" : 5,
+        "keyword": "nautica en madrid",
+        "max_results" : 1000
     },
+    {
+        "keyword": "nautica en barcelona",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en álava",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en albaceria",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en alicante",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en almería",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en asturias",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en ávila",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en badajoz",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en barcelona",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en burgos",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en cáceres",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en cádiz",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en cantabria",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en castellón",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en ciudad real",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en córdoba",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en cuenca",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en gerona",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en granada",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en guadalajara",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en guipúzcoa",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en huelva",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en huesca",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en islas baleares",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en jaén",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en la coruña",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en la rioja",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en las palmas",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en león",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en lleida",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en lugo",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en madrid",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en málaga",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en murcia",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en navarra",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en orense",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en palencia",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en pontevedra",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en salamanca",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en segovia",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en sevilla",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en soria",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en tarragona",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en tenerife",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en teruel",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en toledo",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en valencia",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en valladolid",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en vizcaya",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en zamora",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en zaragoza",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en ibiza",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en formentera",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en mallorca",
+        "max_results" : 1000
+    },
+    {
+        "keyword": "nautica en menorca",
+        "max_results" : 1000
+    }
 ]
 
 number_of_scrapers = 16
